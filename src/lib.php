dc0e322e8142090545a37d69843643fb509304b4
Adrien Baudet <baudetad@gmail.com>
2019-05-25 10:57:56 +0200

    Added primitives and changed code format

--- src/lib.php
+++ src/lib.php
@@ -2,23 +2,52 @@
   include "instance.php";
   $Object = new Instance(null);
   $Object->identity = function($self)
-    {
-      return $self;
-    };
+  {
+    return $self;
+  };
 
   $Primitive = $Object->clone();
   $Primitive->identity = function($self)
-    {
-      return $self->value;
-    };
-  $Primitive->from = function($self, $chaine)
-    {
-      $val = $self->clone();
-      $val->value = $chaine;
-      return $val;
-    };
+  {
+    return $self->value;
+  };
+  $Primitive->from = function($self, $value)
+  {
+    $val = $self->clone();
+    $val->value = $value;
+    return $val;
+  };
 
   $String = $Primitive->clone();
+  $String->from = function($self, string $value)
+  {
+    $val = $self->clone();
+    $val->value = $value;
+    return $val;
+  };
+
+  $Bool = $Primitive->clone();
+  $Bool->from = function($self, bool $value)
+  {
+    $val = $self->clone();
+    $val->value = $value;
+    return $val;
+  };
+
   $Int = $Primitive->clone();
+  $Int->from = function($self, int $value)
+  {
+    $val = $self->clone();
+    $val->value = $value;
+    return $val;
+  };
+
+  $Float = $Primitive->clone();
+  $Float->from = function($self, float $value)
+  {
+    $val = $self->clone();
+    $val->value = $value;
+    return $val;
+  };
 
 ?>
