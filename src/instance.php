a41e8a0d2ab5da38a45e9d1e4304d35d69dcd835
Adrien Baudet <baudetad@gmail.com>
2019-05-28 19:18:48 +0200

    Changed code to maker use og __get and __set and make complete values inheritance

--- src/instance.php
+++ src/instance.php
@@ -4,39 +4,53 @@
    */
   class Instance
   {
+    public $prototype;
+    public $values;
+
     function __construct($prototype)
     {
       $this->prototype = $prototype;
+      $this->values = [];
     }
 
     function __call($name, $args)
     {
-      // foreach($args as $key => $val)
-      // {
-      //   if ($val instanceof Instance)
-      //   {
-      //     $args[$key] = $val->identity();
-      //   }
-      // }
       array_unshift($args, $this);
-      if (isset($this->$name))
+
+      $method = $this->look_for($name);
+      return $method(...$args);
+    }
+
+    function __get($name)
+    {
+      return $this->look_for($name);
+    }
+
+    function __set($name, $value)
+    {
+      return $this->values[$name] = $value;
+    }
+
+    function look_for($name)
+    {
+      if (isset($this->values[$name]))
       {
-        return ($this->$name)(...$args);
+        return $this->values[$name];
       }
       else
       {
         $inst = $this->prototype;
-        while (isset($inst) && (! isset($inst->$name)))
+        while (isset($inst) && (! isset($inst->values[$name])))
         {
           $inst = $inst->prototype;
         }
-        if (isset($inst->$name))
+        if (isset($inst->values[$name]))
         {
-          return ($inst->$name)(...$args);
+          return $inst->values[$name];
         }
         else
         {
-          throw new Exception('Method not found -> ' . $name);
+          throw new Exception('Value not found -> ' . $name);
         }
       }
     }
